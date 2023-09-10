31101d864ffc08d723c9b651c262d886c5535ea6
patricia-vm1 <142701597+patricia-vm1@users.noreply.github.com>
2023-09-10 15:40:03 -0500

    Initial commit (Ch13 up to page 258)

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Placebook2"
+include(":app")
